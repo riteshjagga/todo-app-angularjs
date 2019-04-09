73c9ae7563b2b80752e5cc53640f21513ce2b6e2
Ritesh Jagga <riteshjagga@gmail.com>
2019-04-09 13:47:29 +0530

    Fix deployment issue

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var path = require('path');
 var server = express();
 
 server.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'app/index.html'));
+    res.sendFile(path.join(__dirname, '/app/index.html'));
 });
 
-server.use(express.static(__dirname));
+server.use(express.static(__dirname + '/app'));
 server.listen(process.env.PORT || 3000);
+
+console.log('Node.js server started on ' + (process.env.PORT || 3000));
